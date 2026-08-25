444a4aa90f9afc3cf6235596e5e909690a287403
Pablo de Albuquerque <pablo.cda@gmail.com>
2026-08-25 09:27:51 -0300

    habilito overrides locales de parámetros

--- src/kedro_cic/settings.py
+++ src/kedro_cic/settings.py
@@ -21,8 +21,9 @@ from kedro.config import OmegaConfigLoader  # noqa: E402
 CONFIG_LOADER_CLASS = OmegaConfigLoader
 # Keyword arguments to pass to the `CONFIG_LOADER_CLASS` constructor.
 CONFIG_LOADER_ARGS = {
-      "base_env": "base",
-      "default_run_env": "local",
+    "base_env": "base",
+    "default_run_env": "local",
+    "merge_strategy": {"parameters": "soft"},
 #       "config_patterns": {
 #           "spark" : ["spark*/"],
 #           "parameters": ["parameters*", "parameters*/**", "**/parameters*"],
